8846ae09313607dbfd8fa937a0aaeddfb5a14587
Emmy Bruynseels <emmy.bruynseels@hotmail.com>
2020-07-23 17:01:06 +0200

    index page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormComponent } from './form/form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
+  { path: 'index',                component: FormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
