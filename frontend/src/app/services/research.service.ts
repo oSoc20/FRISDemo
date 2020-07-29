2f064fa5b72d48eb5de47f123dcdafbeb837824b
Emmy Bruynseels <emmy.bruynseels@hotmail.com>
2020-07-29 11:08:54 +0200

    remove baseurldemo

--- frontend/src/app/services/research.service.ts
+++ frontend/src/app/services/research.service.ts
@@ -10,7 +10,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ResearchService {
-  baseurlDemo = environment.baseurlDemo;
+  // baseurlDemo = environment.baseurlDemo;
   baseurlEnricher = environment.baseurl;
   private jsonURL = 'assets/data.json';
 
@@ -20,27 +20,27 @@ export class ResearchService {
     return this.http.get(this.jsonURL);
   }
 
-  getResearch(): Promise<Research> {
-    return new Promise((resolve, reject) => {
-      this.http.get<any>(this.baseurlDemo + '/research').subscribe(result => {
-        console.log(result);
-        let projects: Project[] = [];
-        let publications: Publication[] = [];
-        result.projects.forEach(p => {
-          // tslint:disable-next-line: max-line-length
-          let project = new Project(p.id, p.title.englishTitle, p.title.dutchTitle, p.englishKeywords, p.dutchKeywords, p.abstract.englishAbstract, p.abstract.dutchAbstract);
-          projects.push(project);
-        });
-        result.publications.forEach(p => {
-          let publication = new Publication(p.uuid, p.titleEn, p.titleNl, p.keywordsEn, p.keywordsNl, p.abstractEn, p.abstractNl, p.doi);
-          publications.push(publication);
-        });
-        console.log(projects);
-        console.log(JSON.stringify(new Research(projects, publications)));
-        resolve(new Research(projects, publications));
-      });
-    });
-  }
+  // getResearch(): Promise<Research> {
+  //   return new Promise((resolve, reject) => {
+  //     this.http.get<any>(this.baseurlDemo + '/research').subscribe(result => {
+  //       console.log(result);
+  //       let projects: Project[] = [];
+  //       let publications: Publication[] = [];
+  //       result.projects.forEach(p => {
+  //         // tslint:disable-next-line: max-line-length
+  //         let project = new Project(p.id, p.title.englishTitle, p.title.dutchTitle, p.englishKeywords, p.dutchKeywords, p.abstract.englishAbstract, p.abstract.dutchAbstract);
+  //         projects.push(project);
+  //       });
+  //       result.publications.forEach(p => {
+  //         let publication = new Publication(p.uuid, p.titleEn, p.titleNl, p.keywordsEn, p.keywordsNl, p.abstractEn, p.abstractNl, p.doi);
+  //         publications.push(publication);
+  //       });
+  //       console.log(projects);
+  //       console.log(JSON.stringify(new Research(projects, publications)));
+  //       resolve(new Research(projects, publications));
+  //     });
+  //   });
+  // }
 
   enrichProject(project) {
     return this.http.post<any>(this.baseurlEnricher + '/api/projects/enrich', project);
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseurlDemo: 'http://127.0.0.1:5000',
+  // baseurlDemo: 'http://127.0.0.1:5000',
   baseurl: 'https://fris-enricher.herokuapp.com'
 };
